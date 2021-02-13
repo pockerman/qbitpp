9fe5d66346ee5e6951bc7e7c20b11ba9d51457bf
Alex <a.giavaras@gmail.com>
2021-02-13 17:06:49 +0000

    #8, #9, #12 Various API updates and refactorings

---
+++ include/qbit/gates/gate_type.h
@@ -0,0 +1,19 @@
+#ifndef GATE_TYPE_H
+#define GATE_TYPE_H
+
+namespace qbit {
+
+///
+/// \brief The GateType enum. Useful enumeration of the
+/// gate types used around the simulator
+///
+enum class GateType{
+
+    NONE=0, HADAMARD, HADAMADARD_WITH_MES,
+    IDENTITY, IDENTITY_WITH_MES
+
+};
+
+}
+
+#endif // GATE_TYPE_H
---
+++ include/qbit/gates/q_measurement.h
@@ -0,0 +1,21 @@
+#ifndef Q_MEASUREMENT_H
+#define Q_MEASUREMENT_H
+
+namespace qbit{
+
+
+///
+/// \brief The QMeasurement struct. Models
+/// a measurement applied on a Qubit or
+/// gate or circuit. For example in a layered
+/// QCircuit the output layer consists of QMeasurement
+/// units
+///
+struct QMeasurement
+{
+
+};
+
+}
+
+#endif // Q_MEASUREMENT_H
