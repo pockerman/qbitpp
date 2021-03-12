18fb89d34508164ebaa30f47eeedc576036ab1b5
Alex <a.giavaras@gmail.com>
2021-03-12 15:50:53 +0000

    #26 Deactivate example

--- examples/example_3/exe.cpp
+++ examples/example_3/exe.cpp
@@ -11,30 +11,30 @@
 int main(){
 
     // the circuit to use
-    qbit::circuits::QCircuit circuit(2);
+    //qbit::circuits::QCircuit circuit(2);
 
-    qbit::DynVec<qbit::real_t> state(2, 0.0);
-    state[0] = 1.0;
+    //qbit::DynVec<qbit::real_t> state(2, 0.0);
+    //state[0] = 1.0;
 
     // apply the hadamard operation to
     // qubit 0
-    circuit.add_hadamard_gate(0);
+    //circuit.add_hadamard_gate(0);
 
     // apply the hadamard gate to qubit 1
-    circuit.add_hadamard_gate(1);
+    //circuit.add_hadamard_gate(1);
 
     // add the oracle
-    circuit.add_operation();
+    //circuit.add_operation();
 
     // add the measurement
-    circuit.add_measurement();
+    //circuit.add_measurement();
 
     // execute the circuit
-    auto out = circuit();
+    /*auto out = circuit();
 
     for(auto item:out){
         std::cout<<item<<std::endl;
-    }
+    }*/
 
     return 0;
 }
