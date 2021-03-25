5a0a5033b90fc6607f7093498a8ee2c22c2b7f18
Alex <a.giavaras@gmail.com>
2021-03-25 20:19:08 +0000

    Add dependency on quantum++ library

--- examples/example_1/exe.cpp
+++ examples/example_1/exe.cpp
@@ -2,35 +2,15 @@
 #include "qbit/base/qubit.h"
 #include "qbit/gates/hadamard_gate.h"
 
-
+#include "qpp.h"
 #include <vector>
 #include <iostream>
 
-namespace  {
 
-    using qbit::uint_t;
-    using qbit::real_t;
-    using qbit::gates::HGate;
-    using qbit::Qubit;
-    using qbit::DynVec;
-
-}
 
 int main(){
 
-    // prepare the qubit using the
-    // Hadamard gate
-
-    DynVec<real_t> state(2, 0.0);
-    state[0] = 1.0;
-
-    Qubit q1(state);
-    std::cout<<q1<<std::endl;
-
-    HGate gate(q1);
-
-    // let's compute the transformation
-    auto q2 = gate();
-    std::cout<<q2[0]<<std::endl;
-    return 0;
+    using namespace qpp;
+    std::cout << "Hello Quantum++!\nThis is the |0> state:\n";
+    std::cout << disp(0_ket) << '\n';
 }
--- examples/example_3/exe.cpp
+++ examples/example_3/exe.cpp
@@ -1,6 +1,8 @@
 #include "qbit/base/qubit.h"
 #include "qbit/gates/hadamard_gate.h"
 
+#include "qpp.h"
+
 #include <chrono>
 #include <thread>
 #include <vector>
@@ -10,8 +12,9 @@
 
 namespace example{
 
-template<typename InTp>
-std::vector<qbit::Qubit> deutsch_oracle(const InTp& in1, const InTp& in2){
+using namespace qpp;
+
+ket deutsch_oracle(const ket in){
 
     // randomly select what
     // the orcale implements
@@ -22,7 +25,11 @@ std::vector<qbit::Qubit> deutsch_oracle(const InTp& in1, const InTp& in2){
     auto f0 = distrib(gen);
     auto f1 = distrib(gen);
 
-    auto state_x = in1[0].as_vector();
+    ket result = in;
+    result[0] *= std::pow(-1, f0);
+    result[1] *= std::pow(-1, f1);
+
+    /*auto state_x = in1[0].as_vector();
     state_x[0] *= std::pow(-1, f0);
     state_x[1] *= std::pow(-1, f1);
 
@@ -30,7 +37,8 @@ std::vector<qbit::Qubit> deutsch_oracle(const InTp& in1, const InTp& in2){
 
     // bottom qubit remains as is
     result[0] = qbit::Qubit(state_x);
-    result[1] = in2[0];
+    result[1] = in2[0];*/
+
     return result;
 }
 
@@ -39,8 +47,36 @@ std::vector<qbit::Qubit> deutsch_oracle(const InTp& in1, const InTp& in2){
 
 int main(){
 
+    using namespace qpp;
+    using namespace example;
+    ket q0 = States::get_instance().zero();
+    ket q1 = States::get_instance().one();
+
+    std::cout << "Hello Quantum++!\nThis is the |0> state:\n";
+    std::cout << disp(q0) << '\n';
+    std::cout << disp(q1) << '\n';
+
+    // apply hadamard on both qubits
+    q0 = apply(q0, gt.H, {0});
+    q1 = apply(q1, gt.H, {0});
+
+    std::cout << disp(q0) << '\n';
+    std::cout << disp(q1) << '\n';
+
+    q0 = deutsch_oracle(q0);
+    //q0 = apply(q0, gt.H, {0});
+    auto measured = measure(q0, gt.H, {0});
+
+    std::cout << ">> Measurement result: " << std::get<RES>(measured) << '\n';
+    std::cout << ">> Probabilities: ";
+    std::cout << disp(std::get<PROB>(measured), ", ") << '\n';
+    std::cout << ">> Resulting states:\n";
+    for (auto&& it : std::get<ST>(measured))
+        std::cout << disp(it) << "\n\n";
+
+
     // the two qubit registers
-    qbit::DynVec<qbit::real_t> state(2, 0.0);
+    /*qbit::DynVec<qbit::real_t> state(2, 0.0);
     state[0] = 1.0;
 
     qbit::Qubit q0(state);
@@ -66,7 +102,7 @@ int main(){
     auto out_h2 = h2();
 
     std::cout<<out_h2[0]<<std::endl;
-    std::cout<<"Amplitudes squared: "<<out_h2[0].get_amplitudes_sqr()<<std::endl;
+    std::cout<<"Amplitudes squared: "<<out_h2[0].get_amplitudes_sqr()<<std::endl;*/
 
     return 0;
 }
